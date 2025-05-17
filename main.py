82c685bd787cf50fc6a438ce447a693b6616c42b
gigimoose23 <gigimoosegaming@gmail.com>
2025-05-17 17:37:32 +0100

    eee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2,numpy
+import base64 as x87
 def xyz(v1,v2,v3):
     v5 = (v1)
     setattr(v5, v2, v3)
@@ -6,30 +7,36 @@ numpy.functionThatIs1 = numpy.flip
 xyz(numpy, "xyzcrec1", numpy.functionThatIs1)
 
 cam = cv2.VideoCapture(1)
-bg = None
+x904 = None
 for i in range(50):
-    _, bg = cam.read()
-    
-
+    _, x904 = cam.read()
+u89 = "utf-8"
+cv2.orme = cv2.bitwise_and
+setattr(x87, "u8", getattr(x87, "b64decode"))
 numpy.formArrayFromList = numpy.array
-bg = numpy.xyzcrec1(bg,0)
+x904 = numpy.xyzcrec1(x904,0)
 cv2.functionThatIs1 = cv2.inRange
+xyz(cv2, "xyzcrec2", cv2.functionThatIs1)
 cv2.functionThatIs2 = cv2.imshow
+xyz(cv2, "xyzcrec3", cv2.functionThatIs2)
 cv2.functionThatIs3 = cv2.waitKey
 cv2.functionThatIs4 = cv2.morphologyEx
 cv2.functionThatIs5 = cv2.dilate
 cv2.enumThatIs1 = cv2.MORPH_OPEN
+
+xyz(cv2, "recumpradder", getattr(cv2, x87.u8("b3JtZQ==").decode(u89)))
+xyz(cv2, "dolice", getattr(cv2, x87.u8("YWRkV2VpZ2h0ZWQ=").decode(u89)))
 while True:
-    _, frames = cam.read()
-    frames = numpy.flip(frames,1)
-    frames = cv2.cvtColor(frames, cv2.COLOR_BGR2HSV)
+    _, x932 = cam.read()
+    x932 = numpy.flip(x932,1)
+    hsvx932 = cv2.cvtColor(x932, cv2.COLOR_x904R2HSV)
     lowrangeshades = numpy.formArrayFromList([100,40,40])
     highrangeshades = numpy.formArrayFromList([100,255,255])
-    mask = cv2.functionThatIs1(frames, lowrangeshades, highrangeshades)
+    mask = cv2.xyzcrec2(hsvx932, lowrangeshades, highrangeshades)
 
-    lowrangeshades = numpy.formArrayFromList([180,40,40])
+    lowrangeshades = numpy.formArrayFromList([155,40,40])
     highrangeshades = numpy.formArrayFromList([180,255,255])
-    mask2 = cv2.functionThatIs1(frames, lowrangeshades, highrangeshades)
+    mask2 = cv2.xyzcrec2(hsvx932, lowrangeshades, highrangeshades)
     maskf = mask + mask2
 
     maskf = cv2.functionThatIs4(maskf, cv2.enumThatIs1, numpy.ones((3,3)), None, None, 2)
@@ -37,10 +44,10 @@ while True:
   
     
     anotherVar = cv2.bitwise_not(maskf)
-    bg = cv2.bitwise_and(bg, bg, None, maskf)
-    frames = cv2.bitwise_and(frames, frames, None, maskf)
-    anot = cv2.addWeighted(bg, 1, frames, 1, 0)
-    cv2.functionThatIs2("w", maskf)
+    x904 = cv2.recumpradder(x904, x904, None, maskf)
+    x932 = cv2.recumpradder(x932, x932, None, anotherVar)
+    anot = cv2.dolice(x904, 1, x932, 1, 0)
+    cv2.functionThatIs2("w", anot)
 
     if cv2.functionThatIs3(1) == int(str(int(str(int(str(int(str(int(str(int(str(int(str(int(str(int(str(int("32"))))))))))))))))))):
         break
